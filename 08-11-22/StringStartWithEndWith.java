6f1b4e7705e9aad15313d70e1397d71b8a930558
Prithula-Samanta <111556690+Prithula-Samanta@users.noreply.github.com>
2022-11-09 23:16:58 +0530

    Update StringStartWithEndWith.java

--- 08-11-22/StringStartWithEndWith.java
+++ 08-11-22/StringStartWithEndWith.java
@@ -2,7 +2,7 @@ package Practical;
 
 public class StringStartWithEndWith {
  public static void main(String[] args) {
-	String str = "Abhisek";
+	String str = "Prithula Samanta";
 	System.out.println(str.startsWith("A"));
 	System.out.println(str.endsWith("k"));
 }
